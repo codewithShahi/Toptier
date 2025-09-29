86b0702b36462b25b6aff7433e32ae4727df1e07
hamid-ali <hamidali4218867@gmail.com>
2025-09-29 15:13:49 +0500

    reder issu

--- src/components/themes/layout/components/layoutBase.tsx
+++ src/components/themes/layout/components/layoutBase.tsx
@@ -12,32 +12,12 @@ const LayoutBase = (props: LayoutBaseProps) => {
   const { children, className, type } = props;
 
   const { data, loading, error } = useAppSelector((state) => state.appData);
-  const appDataStatus = data?.app
 
 
   return (
     <LayoutContext.Provider value={{ type }}>
-      {loading ? (
-        // ⏳ Show loading spinner while fetching
-        <div className="fixed inset-0 bg-white dark:bg-gray-900 z-50 flex items-center justify-center">
-          <LoadingSpinner size={33} className='text-red-500'/>
-        </div>
-      ) : error ? (
-        // ❌ Show disabled state if API errored
-        // <AgencyDisabled  />
-        <div className="fixed inset-0 bg-white dark:bg-gray-900 z-50 flex flex-col items-center justify-center p-4 text-center">
-          <h2 className="text-2xl font-semibold mb-4 text-red-600">Error Loading Application</h2>
-          <p className="mb-4 text-gray-700 dark:text-gray-300">There was an error loading the application data. Please try again later.</p>
-          <p className="text-sm text-gray-500 dark:text-gray-400">Error Details: Failed to parse URL </p>
-        </div>
-      ) : appDataStatus ? (
-        // ✅ Agency active → show app
-        <main className={className}>{children}</main>
-      ) : (
-        // 🚫 Agency inactive → disabled state
-        <AgencyDisabled />
-      )}
-       {/* <main className={className}>{children}</main> */}
+
+       <main className={className}>{children}</main>
     </LayoutContext.Provider>
   );
 };
